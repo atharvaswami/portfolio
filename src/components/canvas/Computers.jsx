835cd441de2bce727e89473c6a1a0792ef2f3a54
Atharva Swami <atharvaswami@gmail.com>
2023-11-12 23:13:47 -0800

    changed opacity of the computer canvas

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -60,6 +60,7 @@ const ComputersCanvas = () => {
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
+      style={{ opacity: 0.3 }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
